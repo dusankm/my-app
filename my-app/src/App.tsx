9160b1fe9f2fe5544f30e800eba9b9db85ebc47b
dusan <dusankm95@gmail.com>
2024-08-10 20:36:26 -0400

    test

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>CI/CD Test</p>
+      <p>CI/CD Test </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
